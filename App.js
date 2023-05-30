fd1a79900dc75305d74f0203ee131c8750dad0f0
joacodimaro97 <joacodimaro97@gmail.com>
2023-05-30 13:12:25 -0300

    RN-00 (#1)

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View } from 'react-native';
+import Index from './src/components/Index';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+    <View>
+      <Index/>
     </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
