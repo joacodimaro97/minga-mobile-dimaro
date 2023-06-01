f195a5257f322b4b07c9cdb4acffd8343bc4e775
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-05-31 21:17:06 -0300

    RN-03 funcionando-falta proteger rutas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Text, TextInput, ImageBackground  } from 'react-native';
+import {  Text, ImageBackground, Alert  } from 'react-native';
+import Restart from 'react-native-restart';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const Home = () => {
-    const bgimg = { uri: 'https://i.pinimg.com/564x/85/bb/ef/85bbef2c5b2156a5eb007b01b4779d16.jpg' };
+    const bgimg = { uri: 'https://i.pinimg.com/564x/72/d8/69/72d86997f22ba7891a362a31ee2e2e2d.jpg' };
 
 
     const navigation = useNavigation();
@@ -11,12 +14,24 @@ const Home = () => {
       const handleMangas = () => {
         navigation.navigate('Mangas');
       };
+      const handleLogout = async () => {
+        try {
+          await AsyncStorage.removeItem('token');
+          await AsyncStorage.removeItem('user');
+          Alert.alert('See you soon','The session was successfully closed.')
+          navigation.navigate('Welcome to Minga');
+          Restart();
+        } catch (error) {
+        }
+      };
 
   return (
     <ImageBackground source={bgimg} className='w-screen h-screen flex justify-around items-center ' resizeMode='cover' >
-        <Text className='text-white text-[24px] tracking-[4px] text-center font-bold py-2 ' >Welcome to{'\n'} MINGA Mobile Application</Text>
-        <Text className='text-white text-center text-[22px]  h-[35vh] font-semibold p-4 tracking-[2px] w-full ' >Immerse yourself in the exciting world of Japanese manga with our extensive collection of popular and exciting titles. From epic adventure stories to heartwarming romantic dramas, you'll find something for everyone here.</Text>
-        <Text onPress={handleMangas} className="text-[#ffffffda] bg-[#5f0e5a] rounded-xl w-[60vw] h-[10vh] text-center border border-[#ffffff] text-[30px] font-bold mt-10 py-4 " >Explore Mangas!</Text>
+        <Text className='text-[#ffffffd7] text-[24px] tracking-[4px] text-center font-bold py-6 ' >Welcome to</Text>
+        <Text className="text-[#f5afdc] font-bold text-[80px] tracking-[15px] text-center " >MINGA</Text>
+        <Text className='text-white text-center text-[30px]  h-[35vh] font-semibold p-8 tracking-[2px]  w-full mt-[100px] bg-[#000000bb] ' >Immerse yourself in the  world of Japanese manga with our extensive collection of popular titles.</Text>
+        <Text onPress={handleMangas} className="text-[#ffffffda] bg-[#5d2659] rounded-xl w-[60vw] h-[10vh] text-center border border-[#ffffff] text-[27px] font-bold mt-10 py-4 " >Explore Mangas!</Text>
+        <Text onPress={handleLogout} className="text-[#f8dbdbbb] text-[25px] tracking-[4px] font-bold " >Logout</Text>
     </ImageBackground>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,107 @@
-import React from 'react'
-import { View, Text, TextInput, ImageBackground, Keyboard, TouchableWithoutFeedback  } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, TextInput, Keyboard, TouchableWithoutFeedback, ImageBackground, KeyboardAvoidingView, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+
+const bgimg = { uri: 'https://i.pinimg.com/564x/ad/34/34/ad34346ab249e362341b4d553fe89e3a.jpg' };
 
 const Register = () => {
+  const [nameValue, setNameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [photoValue, setPhotoValue] = useState('');
+  const navigation = useNavigation();
+  
+  let nameRef = useRef();
+  let passwordRef = useRef();
+  let emailRef = useRef();
+  let photoRef = useRef();
+  
+  useEffect(() => {
+    nameRef.current = nameValue;
+    passwordRef.current = passwordValue;
+    emailRef.current = emailValue;
+    photoRef.current = photoValue;
+  }, [nameValue,passwordValue, emailValue, photoValue]);
+
+  const handleRegister = () => {
+    const dataRegister = {
+      name: nameRef.current,
+      password: passwordRef.current,
+      email: emailRef.current,
+      photo: photoRef.current
+      
+    };
+    console.log(dataRegister)
+
+    axios
+      .post('https://minga-mobile-back.onrender.com/api/auth/signup', dataRegister)
+      .then((res) => {
+       Alert.alert('Success', 'Registration was successful! ');
+       navigation.navigate('Welcome to Minga');
+      
+      })
+      .catch((err) => {
+        let error = err.response.data.message;
+        Alert.alert('Error',`${error}`);
+        
+      });
+
+      const dismissKeyboard = () => {
+        Keyboard.dismiss();
+      };
+  
+      Keyboard.addListener('keyboardDidHide', dismissKeyboard);
+  
+      return () => {
+        Keyboard.removeListener('keyboardDidHide', dismissKeyboard);
+      };
+  };
+
   return (
-    <View className='bg-black flex-1' >
-     
-    </View>
-  )
-}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <ImageBackground source={bgimg} className='bg-black flex-1 flex justify-between items-center' resizeMode='cover'>
+        
 
+        
+        <Text className="text-white font-bold text-[28px] w-[90%] text-center mt-10 ">Register and explore the world of mangas:</Text>
+          <View className=" h-[60vh] flex justify-evenly">
+            <TextInput
+              className="text-white bg-[#000000dd] w-[80vw] h-[8vh] rounded-xl p-2 border-[#fff] border-[2px] "
+              placeholder="Register your Name here..."
+              placeholderTextColor="#d1d0d0"
+              onChangeText={(text) => setNameValue(text)}
+            />
+            <TextInput
+              className="text-white bg-[#000000dd] w-[80vw] h-[8vh] rounded-xl p-2 border-[#fff] border-[2px] "
+              placeholder="Register your Password here..."
+              placeholderTextColor="#d1d0d0"
+              onChangeText={(text) => setPasswordValue(text)}
+              secureTextEntry={true}
+            />
+            <TextInput
+              className="text-white bg-[#000000dd] w-[80vw] h-[8vh] rounded-xl p-2 border-[#fff] border-[2px] "
+              placeholder="Register your Email here..."
+              placeholderTextColor="#d1d0d0"
+              onChangeText={(text) => setEmailValue(text.trim())}
+              keyboardType="email-address"
+              
+            />
+            <TextInput
+              className="text-white bg-[#000000dd] w-[80vw] h-[8vh] rounded-xl p-2 border-[#fff] border-[2px] "
+              placeholder="Register your Url Photo here..."
+              placeholderTextColor="#d1d0d0"
+              onChangeText={(text) => setPhotoValue(text)}
+            />
+        <Text onPress={handleRegister} className="text-black text-[30px] font-bold tracking-[6px] bg-[#edececc8] p-2 text-center " >REGISTER</Text>
+          </View>
+        
+          <Text className='text-[#d1d0d0] font-semibold  tracking-[8px] pb-2 ' > -By Joaquin Dimaro-</Text>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
+};
 
-export default Register
+export default Register;
